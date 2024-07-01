2af3b71d720f90fa45488a29eb93e3fc1f0b290a
Colin <mcbolin@hotmail.com>
2024-07-01 20:08:59 +1000

    feat: context provider in place

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 
 let defaultJournalData = [
@@ -26,4 +26,18 @@ export function UseJournalData(){
 // Hook to call the function that edits the global data
 export function useJournalDispatch(){
     return useContext(JournalDispatchContext);
+}
+
+
+export function BlogProvider(props){
+
+    let [exampleState, setExampleState] = useState("Hello from the global level!");
+
+    return(
+        <JournalDataContext.Provider value={exampleState}>
+            <JournalDispatchContext.Provider value={setExampleState}>
+                {props.children}
+            </JournalDispatchContext.Provider>
+        </JournalDataContext.Provider>
+    )
 }
